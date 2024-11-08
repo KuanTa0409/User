b534d5c1ba95c59787c16d8ad60938cfec77e81a
KuanTa <KuanTa@LinKuanTa>
2024-11-09 00:58:55 +0800

    user

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -18,12 +18,12 @@ public class UserController {
 	private List<User> users = new CopyOnWriteArrayList<>();
 	
 	// 首頁
-	@GetMapping("/")                 
+	@GetMapping("/")    // http://localhost:8081/User/user/      
 	public String index(Model model, @ModelAttribute User user) {
 		// 要放在表單裡面的就是ModelAttribute。放在表單以外的資料要渲染呈現的 在model裡面
-		model.addAttribute("_method", "POST"); //有個變數叫_method，去影響表單的隱含hidden欄位的值
-		model.addAttribute("submitButtonName", "新增"); //將按鈕視為變數，不要寫死，由前端的動作 來調整按鈕名稱
-		model.addAttribute("users", users); //列表，所有使用者的資料
+		model.addAttribute("_method", "POST");           //有個變數叫_method，去影響表單的隱含hidden欄位的值
+		model.addAttribute("submitButtonName", "新增");  //將按鈕視為變數，不要寫死，由前端的動作 來調整按鈕名稱
+		model.addAttribute("users", users);             //列表，所有使用者的資料
 		// model.addAttribute("user", user); 可以不用寫，因為@ModelAttribute會將User user物件帶給前端。
 		return "user";
 		// 四個變數_method、submitButtonName、users、user會帶給前端，讓其自行渲染
