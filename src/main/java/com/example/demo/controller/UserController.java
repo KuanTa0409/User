99fa686c0ab2e7c9121e13493fb6e632d2796c9d
KuanTa <KuanTa@LinKuanTa>
2024-11-10 08:12:47 +0800

    MVC

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -25,6 +25,16 @@ public class UserController {
 		model.addAttribute("submitButtonName", "新增");  //將按鈕視為變數，不要寫死，由前端的動作 來調整按鈕名稱
 		model.addAttribute("users", users);             //列表，所有使用者的資料
 		// model.addAttribute("user", user); 可以不用寫，因為@ModelAttribute會將User user物件帶給前端。
+		
+		/* 資料就會被set至表單上，顯示在前端上...data binding(只要改變 物件屬性內容>前端表單內容就會改變)
+		user.setName("Vincent");
+		user.setAge(18);
+		user.setBirth(new Date());  //在前端，value="Mon May 29 19:58:22 CST 2023"
+		user.setEducation("大學");
+		user.setSex("男");
+		user.setInterest(new String[] {"爬山", "飛控"});
+		user.setResume("我的履歷資料:\n1:我的家庭...\n2:我喜歡...");
+		*/
 		return "user";
 		// 四個變數_method、submitButtonName、users、user會帶給前端，讓其自行渲染
 	}
